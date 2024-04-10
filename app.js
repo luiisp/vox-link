aaf5ae179740740b7a681bf1725bc2465a17ca95
scr6w <playerzone06@gmail.com>
2024-04-09 23:58:20 -0300

    add html structure and styles in index + basic endpoints in app.js

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.get('/', (req, res) => {
     res.render('index'); 
 });
 
+app.get('/create-room', (req, res) => {
+    res.render('create'); 
+});
+
+app.get('/room', (req, res) => {
+
+    res.render('room')
+
+});
+
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Running in *${port}`);
